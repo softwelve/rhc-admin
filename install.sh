2bb05099e4174a50996c0395d0adfb5bbbd68741
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-05-15 21:08:05 +0530

    push

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl 
 sudo mkdir -p /etc/apt/keyrings
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -10,4 +10,11 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nginx -y
+
+git clone <repo name>
+cd <name>
+
+sudo cp ./Nginx/default /etc/nginx/sites-available/default
+sudo systemctl restart nginx
+
