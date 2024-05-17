eb5aab1dae1abd8167345dea4d8f0a021cea94d2
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-05-17 18:40:27 +0530

    url added

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'https://admin.riyadhholding.sa/'),
   app: {
     keys: env.array('APP_KEYS'),
   },
