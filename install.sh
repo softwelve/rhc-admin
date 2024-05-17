e2e6279192ae7f7c4706ccdd0a8a182aabf099c6
Haazique Sayyed <haziq.sayyed@gmail.com>
2024-05-17 19:01:58 +0530

    install script updated

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 cd ~
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl 
+
+# Docker 
 sudo mkdir -p /etc/apt/keyrings
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -11,9 +13,20 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nginx -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm -v
+nvm install v20.0.0
+
+# Code 
 git clone <repo name>
 cd <name>
+npm install 
 
 sudo cp ./Nginx/default /etc/nginx/sites-available/default
 sudo systemctl restart nginx
