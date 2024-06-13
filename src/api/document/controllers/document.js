78e7593a2025762a1ce02b1266598a9d2ea17b16
shivansh135 <58308441+shivansh135@users.noreply.github.com>
2024-06-13 11:59:07 +0530

    Create document.js for proxy

---
+++ src/api/document/controllers/document.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+module.exports = {
+  async downloadDocument(ctx) {
+    try {
+      const { document_name } = ctx.request.body;
+      const url = `https://riyadhholding.sharepoint.com/sites/Shamil/_layouts/download.aspx?SourceUrl=https://riyadhholding.sharepoint.com/sites/Shamil/Assets/${document_name}`;
+
+      // Download the file from SharePoint
+      const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'arraybuffer', // This will handle the binary data
+        headers: {
+          // Add any required headers here, such as authentication
+          'Authorization': `Bearer YOUR_ACCESS_TOKEN`,
+        },
+      });
+
+      // Process the downloaded file data if needed
+      const fileData = response.data;
+
+      // Send the data through the pipeline
+      // You can replace the following line with your pipeline logic
+      // For example, you can send the data to another service or process it
+      // Here we simply return the data as a response
+      ctx.send(fileData, response.headers);
+
+    } catch (error) {
+      console.error('Error downloading document:', error);
+      ctx.throw(500, 'Failed to download document');
+    }
+  },
+};
